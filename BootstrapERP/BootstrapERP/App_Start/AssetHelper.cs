e76ce9ab9737d10df488eae6ae0092deebf2e53f
Hadi07009 <hadi.mbstu@gmail.com>
2026-06-21 18:11:45 +0600

    Finalize Hero Section Text

---
+++ BootstrapERP/BootstrapERP/App_Start/AssetHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+
+namespace BootstrapERP.App_Start
+{
+    public static class AssetHelper
+    {
+        public static string VersionedContent(string path)
+        {
+            var filePath = HttpContext.Current.Server.MapPath(path);
+
+            if (File.Exists(filePath))
+            {
+                var version = File.GetLastWriteTime(filePath).Ticks;
+                return path + "?v=" + version;
+            }
+
+            return path;
+        }
+    }
+}
